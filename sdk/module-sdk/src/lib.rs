6391612d743fbae670f16efdf469e1cf329a4df7
Tim Jochen Kicker <tim@kicker.dev>
2026-03-22 12:54:22 +0100

    ui-kit: initial Tauri and SvelteKit setup with surface token system and WindowControls

--- sdk/module-sdk/src/lib.rs
+++
@@ -1,5 +0,0 @@
-// module-sdk exposes a restricted subset of os-sdk for third-party modules.
-// It re-exports only what external developers need, keeping internal
-// os-sdk surfaces unexposed.
-
-pub use os_sdk::event::{EventEmitter, EmitError};
--- sdk/os-sdk/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    prost_build::compile_protos(&["proto/event.proto"], &["proto/"])?;
-    Ok(())
-}
