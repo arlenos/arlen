f02eefc1501a16d9c613ef5d338f015d994791b7
Tim Jochen Kicker <tim@kicker.dev>
2026-06-29 18:12:41 +0200

    dev: build and render the desktop shell bar in the image

--- dev/mkosi/mkosi.build.d/01-install-deps.sh
+++ dev/mkosi/mkosi.build.d/01-install-deps.sh
@@ -19,4 +19,7 @@ apt-get install -y --no-install-recommends \
     ca-certificates curl cmake g++ make protobuf-compiler pkg-config libfuse3-dev \
     libudev-dev libgbm-dev libxkbcommon-dev libegl1-mesa-dev libwayland-dev \
     libinput-dev libdbus-1-dev libsystemd-dev libseat-dev libdisplay-info-dev \
-    libpixman-1-dev
+    libpixman-1-dev \
+    nodejs npm \
+    libwebkit2gtk-4.1-dev libgtk-3-dev libsoup-3.0-dev libjavascriptcoregtk-4.1-dev \
+    libssl-dev libgtk-layer-shell-dev
