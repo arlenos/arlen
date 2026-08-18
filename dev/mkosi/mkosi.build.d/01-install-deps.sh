19c06a2e95d58bd9ed0cfee3c32560484505148a
Tim Jochen Kicker <tim@kicker.dev>
2026-08-18 06:17:54 +0200

    mkosi: the image build had no libheif, and a comment said it did

--- dev/mkosi/mkosi.build.d/01-install-deps.sh
+++ dev/mkosi/mkosi.build.d/01-install-deps.sh
@@ -33,4 +33,5 @@ apt-get install -y --no-install-recommends \
     libpixman-1-dev \
     nodejs npm \
     libwebkit2gtk-4.1-dev libgtk-3-dev libsoup-3.0-dev libjavascriptcoregtk-4.1-dev \
-    libssl-dev libgtk-layer-shell-dev
+    libssl-dev libgtk-layer-shell-dev \
+    libheif-dev
